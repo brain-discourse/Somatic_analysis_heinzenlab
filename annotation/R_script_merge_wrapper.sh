b10ccac233256c0a1d13e2c2bba01016f1ed36d2
brain-discourse <78502397+brain-discourse@users.noreply.github.com>
2025-05-04 18:41:07 -0400

    Delete annotation/R_script_merge_wrapper.sh

--- annotation/R_script_merge_wrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-	#SBATCH -N 1
-	#SBATCH -n 6
-	#SBATCH -p general
-	#SBATCH --nice=30 
-	#SBATCH -t 07-00:00:00
-#SBATCH --mem=40g
-module load r/4.0.1
-R CMD BATCH --no-restore --no-save Rscript.r
