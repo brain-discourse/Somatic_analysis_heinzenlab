b640a5bc6a842526f6db5d49d67b2a9c06493044
brain-discourse <78502397+brain-discourse@users.noreply.github.com>
2025-05-04 18:41:47 -0400

    Delete annotation/wrapper_annovar.sh

--- annotation/wrapper_annovar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for filename in /proj/heinzenlab/users/meethila1/brain_only/new_samples/*_filtered.vcf
-
-do 
-
-sbatch --time=100:00:00 --mem=40g --job-name NGS --wrap="sh /proj/heinzenlab/users/meethila1/brain_only/new_samples/loop_testing.sh $filename"
-sleep 1
-done
